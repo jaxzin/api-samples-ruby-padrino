12115df1aff5296802f1cac4088497fb1d9c60b9
Brian R. Jackson <brian@jaxzin.com>
2012-03-07 00:20:13 -0500

    Initial scaffold via "padrino g project api-samples-ruby-padrino -t shoulda -e haml -c less -s jquery -d activerecord -b"

---
+++ test/test_config.rb
@@ -0,0 +1,15 @@
+PADRINO_ENV = 'test' unless defined?(PADRINO_ENV)
+require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
+
+require 'shoulda/active_record'
+
+class Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    ##
+    # You can handle all padrino applications using instead:
+    #   Padrino.application
+    ApiSamplesRubyPadrino.tap { |app|  }
+  end
+end
