09ad8aa87e2b67e5854366aa74e7860114ab34e6
Brian R. Jackson <brian@jaxzin.com>
2012-03-22 00:30:12 -0400

    Fix #2, switch to using espnrb objects instead of raw json hash where possible.

--- app/controllers/headlines.rb
+++ app/controllers/headlines.rb
@@ -2,10 +2,10 @@ ApiSamplesRubyPadrino.controllers :headlines do
 
   get :index do
     espn = EspnRb.headlines
-    @headlines = espn.all(:top).response['headlines']
-    @accordion_headlines_nba = espn.nba(:top).response['headlines']
-    @accordion_headlines_mlb = espn.mlb(:top).response['headlines']
-    @accordion_headlines_nfl = espn.nfl(:top).response['headlines']
+    @headlines = espn.all(:top)
+    @accordion_headlines_nba = espn.nba(:top)
+    @accordion_headlines_mlb = espn.mlb(:top)
+    @accordion_headlines_nfl = espn.nfl(:top)
     render 'headlines/index'
   end
 
